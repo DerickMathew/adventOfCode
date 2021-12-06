fcaa0f904cc18cd4bb22917a93ba022e11f1324a
Derick Mathew <derick.mathew@everest.engineering>
2021-12-06 14:48:05 +0530

    updating Day 5's solution

--- 2021/05/p2/solution.py
+++ 2021/05/p2/solution.py
@@ -1,16 +1,10 @@
-
 def plotPoints(instructions):
 	plotMap = {}
 	for line in instructions:
 		startStr, endStr = line.split(' -> ')
 		[startX, startY] = [int(n) for n in startStr.split(',')]
 		[endX, endY] = [int(n) for n in endStr.split(',')]
-		if startX == endX:
-			markVerticalLines(plotMap, startX, startY, endX, endY)
-		elif startY == endY:
-			markHorizontalLines(plotMap, startX, startY, endX, endY)
-		elif abs(startX-endX) == abs(startY - endY):
-			markDiagonalLines(plotMap, startX, startY, endX, endY)
+		markLines(plotMap, startX, startY, endX, endY)
 	return plotMap
 
 def incrementKey(pointMap, x, y):
@@ -18,21 +12,21 @@ def incrementKey(pointMap, x, y):
 		pointMap[(x, y)] = 0
 	pointMap[(x, y)] += 1
 
-def markDiagonalLines(pointMap, startX, startY, endX, endY):
-	xIncrement = 1 if startX < endX else -1
-	yIncrement = 1 if startY < endY else -1
-	for index in range(abs(startX-endX) + 1):
-		x = startX + (index * xIncrement)
-		y = startY + (index * yIncrement)
+def getIncrements(start, end):
+	if start == end:
+		return 0
+	if start < end:
+		return 1
+	return -1
+
+def markLines(pointMap, startX, startY, endX, endY):
+	dx = getIncrements(startX, endX)
+	dy = getIncrements(startY, endY)
+	maxDiff = max(abs(startX-endX),  abs(startY-endY))
+	for index in range(maxDiff + 1):
+		x = startX + (index * dx)
+		y = startY + (index * dy)
 		incrementKey(pointMap, x, y)
-
-def markVerticalLines(pointMap, startX, startY, endX, endY):
-	for index in range(min(startY, endY), max(startY, endY) + 1 ):
-		incrementKey(pointMap, startX, index)
-
-def markHorizontalLines(pointMap, startX, startY, endX, endY):
-	for index in range(min(startX, endX), max(startX, endX) + 1 ):
-		incrementKey(pointMap, index, startY)
 
 def getOverLap(pointMap):
 	return len(list(filter(lambda x: pointMap[x] > 1, pointMap.keys())))
